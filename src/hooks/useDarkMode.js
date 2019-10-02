edf9dcb11be7e61baff808f3b2b288c7029b3031
Sheila Eichenberger <grandpoobah@Sheilas-MacBook-Pro.local>
2019-10-02 14:23:10 -0700

    useDarkMode built

--- src/hooks/useDarkMode.js
+++ src/hooks/useDarkMode.js
@@ -1,14 +1,17 @@
 import { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage.js';
 
-export const useDarkMode = (key, initialValue) => {
-    const [darkMode, setDarkMode] = useLocalStorage('darkValue')
+export const useDarkMode = () => {
+    const [darkMode, setDarkMode] = useLocalStorage("darkModeKey", false)
 
     useEffect(() => {
-        if(useLocalStorage == true) {
-            return (
-                
-            )
-        }
-    })
+        
+        if(useLocalStorage.getItem("darkModeKey") == true) {
+                document.querySelector('body').classList.add('dark-mode')
+        } else {
+            document.querySelector('body').classList.remove('dark-mode')
+        };
+    }, [darkMode]);
+
+    return [darkMode, setDarkMode];
 }
